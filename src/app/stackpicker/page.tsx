8e1cc852ec7dd3db4c871091bb1159321848bb7a
Tuomo Ahonen <tuomo.ahonen@live.fi>
2024-11-26 15:32:55 +0200

    trying to fix Vercel problem to pass deployment of app, suspenses for Buttons, that use searchparams and static paths for readfiles, last try for real part 3

--- src/app/stackpicker/page.tsx
+++ src/app/stackpicker/page.tsx
@@ -5,8 +5,8 @@ import StackPickerComponent from './stackPickerComponent';
 //import fakeApiBackendAddons from '@/components/fakeApi/backend_addons.json';
 
 export default async function StackPicker() {
-	const fakeApi = await readFile('/src/components/fakeapi/options_for_stackpicker_v2');
-	const fakeApiBackendAddons = await readFile('/src/components/fakeApi/backend_addons');
+	const fakeApi = await readFile('/src/components/fakeapi/options_for_stackpicker_v2.json');
+	const fakeApiBackendAddons = await readFile('/src/components/fakeApi/backend_addons.json');
 
 	//console.log(fakeApiBackendAddons);
 
