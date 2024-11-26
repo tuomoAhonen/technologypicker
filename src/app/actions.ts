4103580e487262482592e47ab9aa575820449c7d
Tuomo Ahonen <tuomo.ahonen@live.fi>
2024-11-26 15:04:29 +0200

    trying to fix Vercel problem to pass deployment of app, suspenses for Buttons, that use searchparams and static paths for readfiles, last try

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -3,6 +3,11 @@
 import {promises as fs} from 'fs';
 
 export async function readFile(path: any) {
-	const file = await fs.readFile(path, 'utf8');
+	if (process.env.NODE_ENV === 'production') {
+		const file = await fs.readFile(process.cwd() + '.next/server' + path, 'utf8');
+		return JSON.parse(file);
+	}
+
+	const file = await fs.readFile(process.cwd() + path, 'utf8');
 	return JSON.parse(file);
 }
