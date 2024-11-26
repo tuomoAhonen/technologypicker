535e4038d6dbadc32f7f66ebaf8d3fc5306df31a
Tuomo Ahonen <tuomo.ahonen@live.fi>
2024-11-26 14:53:10 +0200

    trying to fix Vercel problem to pass deployment of app, suspenses for Buttons, that use searchparams and static paths for readfiles, part 5

--- src/app/stackpicker/page.tsx
+++ src/app/stackpicker/page.tsx
@@ -5,10 +5,10 @@ import StackPickerComponent from './stackPickerComponent';
 //import fakeApiBackendAddons from '@/components/fakeApi/backend_addons.json';
 
 export default async function StackPicker() {
-	const fakeApi = await readFile(process.cwd() + '/src/components/fakeapi/options_for_stackpicker_v2.json');
-	const fakeApiBackendAddons = await readFile(process.cwd() + '/src/components/fakeApi/backend_addons.json');
+	const fakeApi = await readFile(/*process.cwd() + */ './src/components/fakeapi/options_for_stackpicker_v2.json');
+	const fakeApiBackendAddons = await readFile(/*process.cwd() + */ './src/components/fakeApi/backend_addons.json');
 
-	console.log(fakeApiBackendAddons);
+	//console.log(fakeApiBackendAddons);
 
 	return (
 		<div className='flex flex-col gap-5 p-5'>
